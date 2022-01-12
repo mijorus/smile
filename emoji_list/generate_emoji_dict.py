cfec5fa26b8ebf1f06981b0c2c5d8f663d0af743
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-01-12 01:12:47 +0100

    better dictionary creation logic

--- emoji_list/generate_emoji_dict.py
+++ emoji_list/generate_emoji_dict.py
@@ -9,8 +9,18 @@ from problematic_emojis import problematic
 
 emoji_list = json.load(open(os.path.dirname(__file__) + '/openmoji.json', 'r'))
 
-output = []
+output = {}
 cat = set()
+
+def append_skintone(skintone: dict, base_hex: str):
+    global output
+    for e, o in output.items():
+        if o['hexcode'] == base_hex: 
+            if not 'skintones' in o:
+                o['skintones'] = []
+
+            o['skintones'].append(skintone) 
+
 for i, el in enumerate(emoji_list):
     if problematic.__contains__(el['hexcode']) or el['group'] == 'extras-openmoji':
         continue
@@ -22,7 +32,22 @@ for i, el in enumerate(emoji_list):
         except:
             pass
 
-    output.append(el)
+    if (len(el['skintone_base_hexcode']) > 0) and (el['skintone_base_hexcode'] != el['hexcode']):
+        append_skintone(el, el['skintone_base_hexcode'])
+        continue
+
+    nested_groups = {
+            'food-drink':  'animals-nature',
+            'component':  'symbols',
+            'symbols':  'objects',
+            'people-body':  'smileys-emotion',
+            'extras-unicode': 'symbols',
+    }
+
+    if el['group'] in nested_groups:
+        el['group'] = nested_groups[el['group']]
+
+    output[el['emoji']] = el
     cat.add(el['group'])
 
 
