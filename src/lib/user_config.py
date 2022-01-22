eef57997144c12f7da1b8b36047d61509fb04843
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-01-22 19:22:53 +0100

    created helpers funcs to save custom configuration files

---
+++ src/lib/user_config.py
@@ -0,0 +1,34 @@
+import json
+from gi.repository import GLib
+from .emoji_list import emojis
+
+def remove_config_file():
+    pass
+
+def save_json_config(content: dict or List, filename: str):
+    """Saves in a configuration file"""
+    config_dir = GLib.get_user_config_dir()
+    return GLib.file_set_contents(f"{config_dir}/{filename}.json", json.dumps(content).encode())
+
+def read_json_config(filename: str):
+    """Reads from a configuration file"""
+    config_dir = GLib.get_user_config_dir()
+    config_filename = f"{config_dir}/{filename}.json"
+
+    current_conf_raw = False
+
+    try:
+        current_conf_raw = GLib.file_get_contents(config_filename)
+    except GLib.Error as e:
+        if e.code != GLib.FileError.NOENT:
+            return False
+
+    current_conf_json = current_conf_raw.contents.decode() if current_conf_raw else '{}'
+
+    try:
+        current_conf = json.loads(current_conf_json)
+    except:
+        print('Config file is not readable')
+        return False
+
+    return current_conf
