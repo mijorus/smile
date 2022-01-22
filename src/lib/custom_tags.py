7d50d335eb9abe2bab48aaefd5766be50560ed7c
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-01-22 19:22:53 +0100

    created helpers funcs to save custom configuration files

--- src/lib/custom_tags.py
+++ src/lib/custom_tags.py
@@ -1,30 +1,14 @@
 import json
 from gi.repository import GLib
 from .emoji_list import emojis
+from .user_config import save_json_config, read_json_config
 
 def remove_config_file():
     pass
 
 def set_custom_tags(emoji: str, tags: str):
     """Saves the new tags for a given emoji in a configuration file"""
-    config_dir = GLib.get_user_config_dir()
-    config_filename = f"{config_dir}/custom_tags.json"
-
-    current_conf_raw = False
-
-    try:
-        current_conf_raw = GLib.file_get_contents(config_filename)
-    except GLib.Error as e:
-        if e.code != GLib.FileError.NOENT:
-            return False
-
-    current_conf_json = current_conf_raw.contents.decode() if current_conf_raw else '{}'
-
-    try:
-        current_conf = json.loads(current_conf_json)
-    except:
-        print('Config file is not readable')
-        return False
+    current_conf = read_json_config('custom_tags')
 
     if not emoji in current_conf:
         current_conf[emoji] = {}
@@ -33,12 +17,5 @@ def set_custom_tags(emoji: str, tags: str):
         current_conf[emoji]['tags'] = ''
 
     current_conf[emoji]['tags'] += f', {tags}' if (len(current_conf[emoji]['tags']) > 0) else tags
-    
-    try:
-        result = GLib.file_set_contents(f"{config_dir}/custom_tags.json", json.dumps(current_conf).encode())
-        if not result:
-            raise Exception()
-    except:
-        return False
 
-    current_conf_json = GLib.file_get_contents(config_filename)
+    res = save_json_config(current_conf, 'custom_tags')
