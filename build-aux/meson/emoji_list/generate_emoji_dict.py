0244dbbe71e40ef0a87afe51d365fb57d59e561a
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-01-24 20:56:06 +0100

    store a maximum amout of emojis in history

--- build-aux/meson/emoji_list/generate_emoji_dict.py
+++ build-aux/meson/emoji_list/generate_emoji_dict.py
@@ -26,11 +26,13 @@ def append_skintone(skintone: dict, base_hex: str):
             o['skintones'].append(skintone) 
 
 for i, el in enumerate(emoji_list):
+    # ignore if an emoji is misbehaving
     if problematic.__contains__(el['hexcode']) or el['group'] == 'extras-openmoji':
         continue
 
     if el['unicode']:
         try:
+            # ignore if an emoji is too recent and potentially unsupported
             if float(el['unicode']) >= 11:
                 continue
         except:
@@ -38,11 +40,15 @@ for i, el in enumerate(emoji_list):
 
     if (len(el['skintone_base_hexcode']) > 0) and (el['skintone_base_hexcode'] != el['hexcode']):
         append_skintone(el, el['skintone_base_hexcode'])
+        # skip if we already have the base emoji
         continue
 
     ignored_groups = ['extras-unicode']
     if ignored_groups.__contains__(el['group']):
+        # ignore if the emoji belongs to the aforementioned groups because they create caos and look bad
         continue
+
+    el['annotation'] = el['annotation'].replace('with ', '')
 
     nested_groups = {
         'food-drink':  'animals-nature',
--- src/lib/emoji_history.py
+++ src/lib/emoji_history.py
@@ -8,17 +8,25 @@ history: dict = None
 
 def increament_emoji_usage_counter(button: Gtk.Button):
     global history
-    max_history_size = 0
+    max_history_size = 30
     
     emoji_hexcode = button.hexcode
     history = read_json_config('usage_history')
-    
+    print(len(history))
+
     if not emoji_hexcode in history:
         history[emoji_hexcode] = {}
         history[emoji_hexcode]['count'] = 0
 
+
     history[emoji_hexcode]['count'] += 1
     history[emoji_hexcode]['lastUsage'] = round(time.time())
+    
+    if (len(history) > max_history_size):
+        sorted_h = dict( sorted(history.items(), key=lambda kv:kv[1]['lastUsage']) )
+        older_key = [*sorted_h][0]
+        del history[older_key]
+
     button.recent = True
 
     save_json_config(history, 'usage_history')
