3b426a8d110edb1abe03ea7c15b83626bea80a42
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-02-02 18:19:27 +0100

    fix custom tags not updating immediately

--- src/lib/custom_tags.py
+++ src/lib/custom_tags.py
@@ -6,6 +6,7 @@ from .user_config import save_json_config, read_json_config
 custom_tags_config = None
 
 def set_custom_tags(emoji: str, tags: str):
+    global custom_tags_config
     """Saves the new tags for a given emoji in a configuration file"""
     current_conf = read_json_config('custom_tags')
 
@@ -15,9 +16,10 @@ def set_custom_tags(emoji: str, tags: str):
     if not 'tags' in current_conf[emoji]:
         current_conf[emoji]['tags'] = ''
 
-    current_conf[emoji]['tags'] += f', {tags}' if (len(current_conf[emoji]['tags']) > 0) else tags
+    current_conf[emoji]['tags'] = tags if tags.endswith(',') or (len(tags) == 0) else f'{tags},'
 
     res = save_json_config(current_conf, 'custom_tags')
+    custom_tags_config = current_conf
 
 def get_custom_tags(hexcode: str, cache = False) -> str:
     global custom_tags_config
