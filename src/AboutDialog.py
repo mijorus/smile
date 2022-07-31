9d3966cba19d8d1e4cdc4148e63749f6787133aa
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-07-31 18:05:09 +0200

    added credits for milesj/emojibase

--- src/AboutDialog.py
+++ src/AboutDialog.py
@@ -5,8 +5,8 @@ class AboutDialog(Gtk.AboutDialog):
         Gtk.AboutDialog.__init__(self)
         self.props.program_name = 'Smile'
         self.props.version = kwargs['version']
-        self.props.website = 'https://github.com/mijorus/smile'
+        self.props.website = 'https://smile.mijorus.it'
         self.props.authors = ['Lorenzo Paderi']
-        self.props.copyright = '(C) 2022 Lorenzo Paderi'
+        self.props.copyright = '(C) 2022 Lorenzo Paderi\nLocalized tags by milesj/emojibase,\nlicensed under the MIT License'
         self.props.logo_icon_name = 'it.mijorus.smile'
         self.set_transient_for(parent)
