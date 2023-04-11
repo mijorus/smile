bf0f400da0e5206c9880d60cbe9a723832c9b9f6
Lorenzo Paderi <lorenzo@mijorus.it>
2023-04-11 18:20:12 +0200

    increase font size in when the setting changes

--- src/components/EmojiButton.py
+++ src/components/EmojiButton.py
@@ -7,20 +7,25 @@ from gi.repository import Gtk, Gio  # noqa
 
 
 class EmojiButton(Gtk.Button):
-    def __init__(self, data, **kwargs):
+    def __init__(self, data: dict, **kwargs):
         super().__init__(label=data['emoji'], **kwargs)
 
-        settings = Gio.Settings.new('it.mijorus.smile')
+        self.app_settings = Gio.Settings.new('it.mijorus.smile')
 
         self.emoji_data = data
         self.hexcode = data['hexcode']
         self.history = None
         
         self.base_skintone_widget = None
-        
-        self.emoji_button_css = [settings.get_string('emoji-size-class')]
 
-        if ('skintones' in data) and data['skintones']:
+        self.update_css_classes()
+
+        self.app_settings.connect('changed::emoji-size-class', lambda w, val: self.update_css_classes())
+
+    def update_css_classes(self):
+        self.emoji_button_css = [self.app_settings.get_string('emoji-size-class')]
+
+        if ('skintones' in self.emoji_data) and self.emoji_data['skintones']:
             self.emoji_button_css.append('emoji-with-skintones')
             
         self.set_css_classes(self.emoji_button_css)
