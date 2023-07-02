fd7a70d8f525d4246e19e30b149044b0c08e486b
Lorenzo Paderi <lorenzo@mijorus.it>
2023-07-02 14:19:53 +0200

    check if the extension is installed

--- src/lib/DbusService.py
+++ src/lib/DbusService.py
@@ -1,11 +1,14 @@
 import gi
 from typing import Optional
+from ..utils import portal
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
 from gi.repository import Gtk, Gio, Gdk, Adw, GLib  # noqa
 
+GNOME_EXTENSION_LINK = 'https://extensions.gnome.org/extension/6093/smile-complementary-extension'
+GNOME_EXTENSION_UUID = 'smile-gnome-extension@mijorus.it'
 
 DBUS_SERVICE_INTERFACE = 'it.mijorus.smile'
 DBUS_SERVICE_PATH = '/it/mijorus/smile/actions'
@@ -22,12 +25,20 @@ DBUS_NODE_XML = """
 
 class DbusService():
     dbus_connection = None
+    extension_status = 'not_installed' # installed, not_installed, unavailable 
 
     def __init__(self):
         self.node = Gio.DBusNodeInfo.new_for_xml(DBUS_NODE_XML)
 
+        try:
+            inter = portal('org.gnome.Shell.Extensions', 'org.gnome.Shell.Extensions', '/org/gnome/Shell/Extensions')
+            installed_extensions = [str(k) for k in inter.ListExtensions().keys()]
+            DbusService.extension_status = 'installed' if GNOME_EXTENSION_UUID in installed_extensions else 'not_installed'
+        except Exception as e:
+            DbusService.extension_status = 'unavailable'
+
     def connect(self):
-        if not self.dbus_connection:
+        if (DbusService.extension_status != 'unavailable') and (not self.dbus_connection):
             Gio.bus_own_name(
                 Gio.BusType.SESSION,
                 DBUS_SERVICE_INTERFACE,
