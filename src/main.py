fd99d9dbd334d01c9881e75d7fd8b3928d9f2a61
Lorenzo Paderi <lorenzo@mijorus.it>
2023-07-06 12:07:24 +0200

    fixed modal not showing for some users

--- src/main.py
+++ src/main.py
@@ -71,9 +71,10 @@ class Smile(Adw.Application):
 
                 last_run_version = self.settings.get_string('last-run-version').replace('.', '')
                 last_run_version = int(last_run_version if len(last_run_version) else '-1')
+                print(last_run_version  )
 
                 # any migration scripts should run here...
-                if last_run_version > 0 and last_run_version < 250 and DbusService.extension_status != 'unavailable':
+                if last_run_version > 0 and last_run_version < 240:
                     dialog = Adw.MessageDialog.new(
                         self.window,
                         _('Pro tip!'),
