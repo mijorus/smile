ed297f581520e05899f8f152288ac326a4d65c69
Lorenzo Paderi <lorenzo@mijorus.it>
2023-12-16 17:46:15 +0100

    fixed startup warning

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,7 @@ def tag_list_contains(tags: str, q: str) -> bool:
 
 # thank you mate ❤️
 # https://github.com/gtimelog/gtimelog/blob/6e4b07b58c730777dbdb00b3b85291139f8b10aa/src/gtimelog/main.py#L159
-def make_option(long_name, short_name=None, flags=0, arg=GLib.OptionArg.NONE, arg_data=None, description=None, arg_description=None):
+def make_option(long_name, short_name=None, flags=0, arg=0, arg_data=None, description=None, arg_description=None):
     # surely something like this should exist inside PyGObject itself?!
     option = GLib.OptionEntry()
     option.long_name = long_name.lstrip('-')
